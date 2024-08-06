aa12b283a3b2c2d11e92dcbd44da30bdd7e0f1f5
zyadfallatah <zyadfallatah@gmail.com>
2024-08-06 06:39:48 +0300

    Theme Component Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Theme from "./components/Theme";
+
 function App() {
-  return <div className=" text-blue-color">Name</div>;
+  return (
+    <div className=" text-blue-color">
+      <Theme />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Theme.tsx
@@ -0,0 +1,40 @@
+import sun from "../assets/icon-sun.svg";
+import moon from "../assets/icon-moon.svg";
+import { useEffect, useState } from "react";
+
+const THEME = "dark";
+
+const Theme = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleThemeMode = function () {
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem(THEME, JSON.stringify(!isDarkMode));
+
+    document.body.classList.toggle(THEME);
+  };
+
+  useEffect(() => {
+    const isLocalThemeDefined: boolean = JSON.parse(
+      localStorage.getItem(THEME)!
+    );
+
+    if (isLocalThemeDefined) {
+      setIsDarkMode(isLocalThemeDefined);
+      const isLocalThemeDark = isLocalThemeDefined;
+      document.body.classList.add(isLocalThemeDark ? THEME : "");
+      return;
+    }
+
+    localStorage.setItem(THEME, JSON.stringify(true));
+  }, []);
+
+  return (
+    <div onClick={handleThemeMode} className=" flex gap-3">
+      <h2>{isDarkMode ? "LIGHT" : "DARK"}</h2>
+      <img src={isDarkMode ? sun : moon} alt="" />
+    </div>
+  );
+};
+
+export default Theme;
