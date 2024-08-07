e63a6cb20407503693e57107e2f7b6efa248d799
zyadfallatah <zyadfallatah@gmail.com>
2024-08-07 06:23:43 +0300

    Layout centered by grid

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -7,7 +7,7 @@ const User = () => {
   return (
     <main
       className="grid grid-cols-[77px_1fr] md:grid-cols-[117px_1fr] gap-x-[1.1875rem]  md:gap-x-10 mt-4 
-    bg-white-alt-color dark:bg-black-alt-color px-6 py-8 lg:px-12"
+    bg-white-alt-color dark:bg-black-alt-color px-6 py-8 lg:px-12 rounded-[10px]"
     >
       <UserAccount />
       <GridLayout>
